b14017c0579608afa331206d84f364caff64fb30
danidzah <danielaoliveirah01@gmail.com>
2023-03-06 14:30:47 -0300

    Autenticação com React

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import './Login.css';
 import { Grid, Typography, TextField, Button } from '@material-ui/core';
 import { Box } from '@mui/material';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useLocalStorage from 'react-use-localstorage';
+import UserLogin from '../../models/UserLogin';
+import { login } from '../../services/Service';
+
+
 
 function Login() {
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: '',
+            foto: '',
+            token: ''
+        }
+        )
+
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+
+            useEffect(()=>{
+                if(token != ''){
+                    navigate('/home')
+                }
+            }, [token])
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                await login('/usuarios/logar', userLogin, setToken)
+
+                alert('Usuário logado com sucesso!');
+            }catch(error){
+                alert('Dados do usuário inconsistentes. Erro ao logar!');
+            }
+        }
+
     return (
         //container principal
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20} >
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component="h3" align="center" style={{ fontWeight: "bold" }}>Entrar</Typography>
-                        <TextField id='usuario' label="usuário" variant="outlined" name="usuario" margin="normal" fullWidth />
-                        <TextField id='senha' label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
+                        <TextField  value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label="usuário" variant="outlined" name="usuario" margin="normal" fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
                         <Box marginTop={2} textAlign="center">
-                            <Link to="/home" className='text-decorator-none'>
+                            
+
                                 <Button type="submit" variant="contained" color="primary" >
                                     Logar
                                 </Button>
-                            </Link>
+                    
                         </Box>
                     </form>
                     <Box display='flex' justifyContent="center" marginTop={2}>
@@ -38,7 +82,7 @@ function Login() {
                 </Box>
             </Grid>
             <Grid xs={6} style={{
-                backgroundImage: `url(https://leaftaxconsultants.com/wp-content/uploads/2020/07/civil-engineering-min-768x512.jpg)`,
+                backgroundImage: 'url(https://leaftaxconsultants.com/wp-content/uploads/2020/07/civil-engineering-min-768x512.jpg)',
                 backgroundRepeat: "no-repeat", width: "100vh", minHeight: "100vh", backgroundSize: "cover", background: "cover", backgroundPosition: "center"
             }}>
 
